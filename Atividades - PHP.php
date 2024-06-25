f1986e4953641364678b6356649a7bfe2b1a7c03
Jasmineggril <jasminedesaraujo@gmail.com>
2024-06-25 10:53:40 -0300

    Atividades - PHP
    Monte um formulário com as seguintes opções abaixo, estarei deixando o código de exemplo
    
    - Nome
    - Nome de Usuário
    - E-mail
    - CPF:
    - Data de Nascimento:
    - Sexo
    - Endereço
    
    DENTRO DO INDEX.PHP
    
    <!DOCTYPE html>
    <html lang="en">
    
    <head>
        <meta charset="UTF-8">
        <meta http-equiv="X-UA-Compatible" content="IE=edge">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <link rel="stylesheet" href="style.css">
        <title>Document</title>
    </head>
    
    <body>
        <form action="foo.php" method="post">
            Nome: <input type="text" name="username" /><br />
            Email: <input type="email" name="email" /><br />
            <input type="submit" value="Me aperte!" />
        </form>
    </body>
    
    </html>
    
    dentro do foo.php
    
    <?php
    echo $_POST['username'];
    echo " / ";
    echo $_POST['email'];
    ?>

---
+++ Atividades - PHP.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Atividades-PHP.css">
+    <title>Atividades - PHP</title>
+</head>
+
+<body>
+    <h1>Formulário</h1>
+    <form action="Atividades-PHP.php" method="post">
+        Nome: <input type="text" name="username" /><br />
+        Nome de Usuário: <input type="text" name="ursernamename"/><br />
+        Email: <input type="email" name="email" /><br />
+        CPF: <input type="number" name="number" /><br />
+        Data de Nascimento: <input type="number" name="number" /><br />
+        Sexo: <input type="text" name="text" /><br />
+        Endereço: <input type="text" name="texty" /><br />
+        <input type="submit" value="CADASTRAR!" />
+    </form>
+</body>
+
+</html>
+
+
+
+
+
---
+++ Atividades-PHP.php
@@ -0,0 +1,18 @@
+<?php
+echo $_POST['username'];
+echo " / ";
+echo $_POST['usernamename']; 
+echo "/";
+echo $_POST['email'];
+echo " / ";
+echo $_POST['CPF'];
+echo " / ";
+echo $_POST['datadenascimento'];
+echo " / ";
+echo $_POST['sexo'];
+echo " / ";
+echo $_POST['endereco'];
+
+?>
+
+
